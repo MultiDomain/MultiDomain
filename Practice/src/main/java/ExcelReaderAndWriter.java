ee218e5900017c3fc17a94148a940877b939552f
msa <mohammed.arefin@gmail.com>
2018-11-18 15:09:46 -0500

    update

--- Practice/src/main/java/ExcelReaderAndWriter.java
+++ Practice/src/main/java/ExcelReaderAndWriter.java
@@ -1,8 +1,16 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+
 public class ExcelReaderAndWriter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        File file = new File("/home/sun/IdeaProjects/MultiDomain/Generic/ExcelFiles/execl2003.xls");
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        HSSFWorkbook 
     }
-
 
 }
