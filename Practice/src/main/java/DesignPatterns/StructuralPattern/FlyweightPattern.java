6ad348f7f7f0a2bce348bc6f8ce7af2472ca40c7
msarefin <mohammed.arefin@gmail.com>
2018-12-24 19:06:35 -0500

    update

---
+++ Practice/src/main/java/DesignPatterns/StructuralPattern/FlyweightPattern.java
@@ -0,0 +1,19 @@
+package DesignPatterns.StructuralPattern;
+
+import java.util.HashMap;
+
+interface Shapes{
+    void draw();
+}
+
+class Circles implements Shapes{
+
+    @Override
+    public void draw() {
+
+    }
+}
+
+
+public class FlyweightPattern {
+}
