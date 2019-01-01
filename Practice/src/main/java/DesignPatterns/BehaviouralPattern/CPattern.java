6c9319dd304f16e78dd44f9d3f886a65cc87a59d
msarefin <mohammed.arefin@gmail.com>
2019-01-01 17:25:52 -0500

    update

---
+++ Practice/src/main/java/DesignPatterns/BehaviouralPattern/CPattern.java
@@ -0,0 +1,130 @@
+package DesignPatterns.BehaviouralPattern;
+
+import org.apache.commons.collections4.functors.OnePredicate;
+
+interface ElectronicDevice {
+    public void on();
+
+    public void off();
+
+    public void volumeUp();
+
+    public void volumeDown();
+}
+
+interface Command {
+    public void execute();
+}
+
+class Television implements ElectronicDevice {
+
+    private int Volume = 0;
+
+
+    @Override
+    public void on() {
+        System.out.println("TV is ON ");
+    }
+
+    @Override
+    public void off() {
+        System.out.println("TV is OFF");
+    }
+
+    @Override
+    public void volumeUp() {
+        Volume++;
+        System.out.println("Volume is UP");
+    }
+
+    @Override
+    public void volumeDown() {
+        Volume--;
+        System.out.println("Volume is DOWN");
+    }
+}
+
+class TurnTVOn implements Command {
+
+    ElectronicDevice theDevice;
+
+    public TurnTVOn(ElectronicDevice newDevice) {
+        theDevice = newDevice;
+    }
+
+    @Override
+    public void execute() {
+
+        theDevice.on();
+    }
+}
+
+class TurnTVOff implements Command {
+    ElectronicDevice theDevice;
+
+    public TurnTVOff(ElectronicDevice newDevice) {
+        theDevice = newDevice;
+    }
+
+    @Override
+    public void execute() {
+        theDevice.off();
+    }
+}
+
+class TurnTVUp implements Command {
+    ElectronicDevice theDevice;
+
+    public TurnTVUp(ElectronicDevice newDevice) {
+        theDevice = newDevice;
+    }
+
+    @Override
+    public void execute() {
+        theDevice.volumeUp();
+    }
+}
+
+class TurnTvDown implements Command {
+    ElectronicDevice theDevice;
+
+    public TurnTvDown(ElectronicDevice newDevice) {
+        theDevice = newDevice;
+    }
+
+    @Override
+    public void execute() {
+        theDevice.volumeDown();
+    }
+}
+
+class DeviceButton {
+    Command theCommand;
+
+    public DeviceButton(Command newCommand) {
+        theCommand = newCommand;
+    }
+
+    public void press(){
+        theCommand.execute();
+    }
+}
+
+class TVRemote{
+    public static ElectronicDevice getDevice(){
+        return new Television();
+    }
+}
+
+
+public class CPattern {
+
+    public static void main(String[] args) {
+        ElectronicDevice newDevice = TVRemote.getDevice();
+        TurnTVOn onCommand = new TurnTVOn(newDevice);
+        DeviceButton onPress = new DeviceButton(onCommand);
+        onPress.press();
+
+    }
+
+}
--- Practice/src/main/java/DesignPatterns/BehaviouralPattern/ChainOfResponsibilityPattern.java
+++ Practice/src/main/java/DesignPatterns/BehaviouralPattern/ChainOfResponsibilityPattern.java
@@ -22,62 +22,64 @@ abstract class AbstractLogger {
             write(message);
         }
         if (nextLogger != null) {
-            nextLogger.logMessage(level, message);
+            nextLogger.logMessage(level, message); //recursive
         }
     }
 
     abstract protected void write(String message);
 }
 
-class ConsoleLogger extends AbstractLogger{
-    public ConsoleLogger(int level){
+class ConsoleLogger extends AbstractLogger {
+    public ConsoleLogger(int level) {
         this.level = level;
     }
+
     @Override
     protected void write(String message) {
-        System.out.println("Standard Console :: Logger: "+ message);
+        System.out.println("Standard Console :: Logger: " + message);
     }
 }
 
-class ErrorLogger extends AbstractLogger{
-    public ErrorLogger(int level){
+class ErrorLogger extends AbstractLogger {
+    public ErrorLogger(int level) {
         this.level = level;
     }
 
     @Override
     protected void write(String message) {
-        System.out.println("File :: Logger: "+ message);
+        System.out.println("Error :: Logger: " + message);
     }
 }
 
-class FileLogger extends AbstractLogger{
-    public FileLogger(int level){
+class FileLogger extends AbstractLogger {
+    public FileLogger(int level) {
         this.level = level;
     }
 
     @Override
     protected void write(String message) {
-        System.out.println("File :: Logger: "+ message);
+        System.out.println("File :: Logger: " + message);
     }
 }
 
 
 public class ChainOfResponsibilityPattern {
-    private  static AbstractLogger getChainLogger(){
-        AbstractLogger erroLogger = new ErrorLogger(AbstractLogger.ERROR);
-        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
-        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
 
-        erroLogger.setNextLogger(fileLogger);
-        fileLogger.setNextLogger(consoleLogger);
+    private static AbstractLogger getChainLogger() {
+        AbstractLogger erroLogger = new ErrorLogger(AbstractLogger.ERROR); //3
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG); //2
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO); //1
+
+        erroLogger.setNextLogger(fileLogger); //3(2)
+        fileLogger.setNextLogger(consoleLogger); //2(1)
 
         return erroLogger;
     }
 
     public static void main(String[] args) {
         AbstractLogger loggerChain = getChainLogger();
-        loggerChain.logMessage(AbstractLogger.INFO,"This is an information.");
-        loggerChain.logMessage(AbstractLogger.DEBUG,"This is a debug level information.");
-        loggerChain.logMessage(AbstractLogger.ERROR,"This is an error infromation.");
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+        loggerChain.logMessage(AbstractLogger.DEBUG, "This is a debug level information.");
+        loggerChain.logMessage(AbstractLogger.ERROR, "This is an error infromation.");
     }
 }
